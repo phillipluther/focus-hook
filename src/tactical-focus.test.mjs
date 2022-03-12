2c420b5f7522367db4a600c04ca9f6606f105c1f
Phillip Luther <dev@phillipluther.com>
2022-03-12 03:19:28 -0800

    Add test env

--- src/tactical-focus.test.mjs
+++ src/tactical-focus.test.mjs
@@ -1,10 +1,47 @@
 import assert from 'assert';
+import jsdomGlobal from 'jsdom-global';
+import tacticalFocus from './tactical-focus.mjs';
 
-try {
-  assert.strictEqual(1, 1, 'Tests forthcoming');
-  console.log('Ok');
-} catch (error) {
-  if (error instanceof AssertionError) {
-    console.error(error);
-  }
+let rootEl;
+let classHook;
+
+function mockKeydown(key = 'Tab') {
+  const event = new window.KeyboardEvent('keydown', { key });
+  window.dispatchEvent(event);
 }
+
+function mockMousedown(type = 'mousedown') {
+  const event = new window.MouseEvent(type);
+  window.dispatchEvent(event);
+}
+
+before(() => {
+  jsdomGlobal();
+});
+
+beforeEach(() => {
+  rootEl = document.body;
+  classHook = 'tactical-focus';
+});
+
+describe('tactical-focus', () => {
+  it('applies no class hook on init', () => {
+    tacticalFocus();
+    assert.strictEqual(rootEl.classList.contains(classHook), false);
+  });
+
+  it('applies a class hook on TAB press', () => {
+    tacticalFocus();
+    mockKeydown();
+
+    assert.strictEqual(rootEl.classList.contains(classHook), true);
+  });
+
+  it('removes a class hook on mouse click', () => {
+    tacticalFocus();
+    mockKeydown(); // known-good from previous test
+    mockMousedown();
+
+    assert.strictEqual(rootEl.classList.contains(classHook), false);
+  });
+});
